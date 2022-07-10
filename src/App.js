babccd50153fb8d416683140a1f27403e3e0f5b2
sdshuvo98 <hellosdshuvo@gamil.com>
2022-07-10 09:51:54 +0600

    created road map

--- src/App.js
+++ src/App.js
@@ -9,3 +9,15 @@ function App() {
 }
 
 export default App;
+
+
+/**Road map
+ * 1. install packages
+ * 2. constants define
+ * 3. async action creator
+ * 4. reducer
+ * 5. create store
+ * 6. provide store
+ * 7. use store
+ * 8. adding css
+ */
